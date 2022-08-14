021ead6ca5eda8fb15f48f1bb31370264c1f874d
Matthias Meyn <matthiasmeyn@gmx.de>
2022-08-15 01:35:34 +0200

    slider

--- tkk_sudoku.py
+++ tkk_sudoku.py
@@ -4,11 +4,12 @@ from tkinter import *
 from sudokusolver import SudokuSolver
 import template_sudokus as ts
 
-#TODO: highlight top of stack?
-#TODO: Buttons: Solve, Speed, Stop, Import, Import from String
-#TODO: Adjust Window Size
-#TODO: Adjust Fontsize
-#TODO: Backtracking solve
+
+# TODO: highlight top of stack?
+# TODO: Buttons: Solve, Speed, Stop, Import, Import from String
+# TODO: Adjust Window Size
+# TODO: Adjust Fontsize
+# TODO: Backtracking solve
 class Gui:
     def __init__(self, root):
         self.sudoku = SudokuSolver(ts.hard_sudoku)
@@ -16,10 +17,19 @@ class Gui:
         self.root.title("SudokuSolver")
         self.root.geometry("1200x900")
 
-        self.time_delay = 100
+        self.stop = False
+        self.time_delay = 1
         self.labels = self.initial()
 
         self.initialize_buttons()
+
+        self.slider = tk.Scale(self.root,
+                               from_=1,
+                               to=2000,
+                               label='Solve/ms',
+                               orient='horizontal',
+                               command=self.slider_change)
+        self.slider.grid(column=200, row=4, sticky='ne')
 
     def initial(self):
         labels = []
@@ -37,7 +47,7 @@ class Gui:
                                  width=8,
                                  relief='solid',
                                  font='Arial 12 bold'
-                                )
+                                 )
                 block.grid(row=i, column=j, pady=(0, pady), padx=(0, padx))
                 row.append(block)
             labels.append(row)
@@ -50,10 +60,13 @@ class Gui:
                 if isinstance(cell_content, list):
                     label.config(fg='red')
                     cell_content = self.clues_to_str(cell_content)
+                if isinstance(cell_content, int):
+                    label.config(fg='black')
                 label["text"] = cell_content
         if self.sudoku.is_solved():
             print('solved')
-        self.root.after(self.time_delay, self.update_labels)
+        if not self.stop:
+            self.root.after(self.time_delay, self.update_labels)
 
     def clues_to_str(self, clues_list):
         """ returns a string in form of '1 2 3\n4 5 6\n7 8 9', but replacing missing clues with space"""
@@ -69,7 +82,22 @@ class Gui:
                 string += '   '
         return string
 
+    def stop_function(self):
+        self.stop = True
+
+    def start_function(self):
+        self.stop = False
+        self.root.after(self.time_delay, self.update_labels)
+
+    def slider_change(self, var):
+        self.time_delay = self.slider.get()
+
     def initialize_buttons(self):
         solve_button = tk.Button(self.root, text="next", command=self.update_labels)
         solve_button.grid(column=200, row=1, sticky='ne')
-        self.root.bind('<Button-1>', self.update_labels)
+
+        stop_button = tk.Button(self.root, text="Stop", command=self.stop_function)
+        stop_button.grid(column=200, row=2, sticky='ne')
+
+        start_button = tk.Button(self.root, text="Start", command=self.start_function)
+        start_button.grid(column=200, row=3, sticky='ne')
